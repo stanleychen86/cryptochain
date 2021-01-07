368ab0c55521e4950cace01dd79c2b1642834686
stanleychen86 <47363544+stanleychen86@users.noreply.github.com>
2021-01-07 01:48:43 -0600

    Initial commit

---
+++ block.js
@@ -0,0 +1,13 @@
+class Block {
+    constructor(timestamp, lastHash, hash, data){
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+}
+
+
+const block1 = new Block();
+
+
